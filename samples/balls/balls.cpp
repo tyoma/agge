1994c0911bfddfabddf81689e25adcfc940d82dd
Artem G <artem.gevorkyan@gmail.com>
2018-08-02 20:26:57 -0600

    Poly-queue library is used for balls queued sample
    - async::wait_completion implemented - theoretically the sample should run on Android now.

--- samples/balls/balls.cpp
+++ samples/balls/balls.cpp
@@ -2,10 +2,10 @@
 
 #include <agge/clipper.h>
 #include <agge/filling_rules.h>
-#include <agge/queue.h>
 #include <agge/rasterizer.h>
 #include <agge/renderer_parallel.h>
 #include <memory>
+#include <poly-queue/circular.h>
 #include <src/agge/hybrid_event.h>
 #include <src/agge/thread.h>
 #include <tests/agge/mt.h>
@@ -36,7 +36,7 @@ namespace
 		async(count_t n);
 
 		rasterizer_type* acquire();
-		
+
 		template <typename SurfaceT, typename BlenderT>
 		void submit(rasterizer_type *rasterizer_, SurfaceT &surface, rect_i *window, const BlenderT &blender);
 
@@ -53,14 +53,16 @@ namespace
 
 	private:
 		renderer_type _renderer;
-		agge::queue<rendition_pack> _unsorted;
-      hybrid_event _unsorted_ready;
-      agge::queue<rendition_pack> _sorted;
-      hybrid_event _sorted_ready;
-      agge::queue<rasterizer_type *> _free;
-      hybrid_event _free_ready;
-      thread _sorter_thread;
+		pq::circular_buffer<rendition_pack> _unsorted;
+		hybrid_event _unsorted_ready;
+		pq::circular_buffer<rendition_pack> _sorted;
+		hybrid_event _sorted_ready;
+		pq::circular_buffer<rasterizer_type *> _free;
+		hybrid_event _free_ready;
+		hybrid_event _complete;
+		thread _sorter_thread;
 		thread _renderer_thread;
+		int _n;
 	};
 
 #pragma pack(1)
@@ -77,40 +79,44 @@ namespace
 		{	ren(_surface, 0, *this->rasterizer, _blender, winding<>());	}
 
 	public:
-		rasterizer_type *rasterizer;
+		rasterizer_type * rasterizer;
 
 	private:
 		void operator =(const rendition_pack&);
 
 	private:
-		platform_bitmap &_surface;
+		platform_bitmap & _surface;
 		char b[8];
 		const platform_blender_solid_color _blender;
 	};
 
 	template <typename RasterizerT>
 	async<RasterizerT>::async(count_t n)
-		: _renderer(2), _sorter_thread(&sorter_proc, this), _renderer_thread(&rendition_proc, this)
+		: _renderer(2), _unsorted(10000), _sorted(10000), _sorter_thread(&sorter_proc, this), _renderer_thread(&rendition_proc, this), _n(n)
 	{
 		while (n--)
 		{
-         auto p = new rasterizer_type;
-         unique_ptr<rasterizer_type> ras(p);
+			auto p = new rasterizer_type;
+			unique_ptr<rasterizer_type> ras(p);
 
-         _free.produce(p, [] (int) {});
-         ras.release();
+			_free.produce(p, [](int) {});
+			ras.release();
 		}
+		_complete.signal();
 	}
 
 	template <typename RasterizerT>
-   RasterizerT *async<RasterizerT>::acquire()
+	RasterizerT *async<RasterizerT>::acquire()
 	{
-      rasterizer_type *ras = 0;
+		rasterizer_type *ras = 0;
 
-      _free.consume([&] (rasterizer_type *o) { ras = o; }, [this] (int n) {
-         if (n == -1)
-            _free_ready.wait();
-      });
+		_free.consume([&](rasterizer_type *o) { ras = o; }, [this](int n) {
+			if (n == 0)
+				_free_ready.wait();
+			if (n == _n)
+				_complete.wait();
+			return true;
+		});
 		return ras;
 	}
 
@@ -120,10 +126,17 @@ namespace
 	{
 		rendition_pack pack(rasterizer_, surface, window, blender);
 
-		_unsorted.produce(pack, [this] (int n) {
-         if (!n)
-            _unsorted_ready.signal();
-      });
+		_unsorted.produce(pack, [this](int n) {
+			if (-1 == n)
+				_unsorted_ready.signal();
+		});
+	}
+
+	template <typename RasterizerT>
+	void async<RasterizerT>::wait_completion()
+	{
+		_complete.wait();
+		_complete.signal(); // acquire & wait_completion must be called from the same thread/queue.
 	}
 
 	template <typename RasterizerT>
@@ -132,16 +145,17 @@ namespace
 		async *self = static_cast<async *>(p);
 		for (;;)
 		{
-			self->_unsorted.consume([self] (rendition_pack &o) {
+			self->_unsorted.consume([self](rendition_pack &o) {
 				o.rasterizer->sort();
-				self->_sorted.produce(o, [self] (int n) {
-               if (!n)
-                  self->_sorted_ready.signal();
-            });
-			}, [self] (int n) {
-            if (n == -1)
-               self->_unsorted_ready.wait();
-         });
+				self->_sorted.produce(o, [self](int n) {
+					if (-1 == n)
+						self->_sorted_ready.signal();
+				});
+			}, [self](int n) {
+				if (n == 0)
+					self->_unsorted_ready.wait();
+				return true;
+			});
 		}
 	}
 
@@ -151,16 +165,19 @@ namespace
 		async *self = static_cast<async *>(p);
 		for (;;)
 		{
-			self->_sorted.consume([self] (rendition_pack &o) {
+			self->_sorted.consume([self](rendition_pack &o) {
 				o.render(self->_renderer);
-				self->_free.produce(o.rasterizer, [self] (int n){
-               if (!n)
-                  self->_free_ready.signal();
-            });
+				self->_free.produce(o.rasterizer, [self](int n) {
+					if (-1 == n)
+						self->_free_ready.signal();
+					if (self->_n - 1 == n)
+						self->_complete.signal();
+				});
 			}, [self](int n) {
-            if (n == -1)
-               self->_sorted_ready.wait();
-         });
+				if (n == 0)
+					self->_sorted_ready.wait();
+				return true;
+			});
 		}
 	}
 
@@ -180,7 +197,7 @@ namespace
 			const rect_i area = { 0, 0, static_cast<int>(surface.width()), static_cast<int>(surface.height()) };
 
 			stopwatch(counter);
-				fill(surface, area, platform_blender_solid_color(255, 255, 255));
+			fill(surface, area, platform_blender_solid_color(255, 255, 255));
 			timings.clearing += stopwatch(counter);
 
 			for (vector<ball>::iterator i = _balls.begin(); i != _balls.end(); ++i)
@@ -191,14 +208,15 @@ namespace
 			{
 				ellipse e(i->x, i->y, i->radius, i->radius);
 				platform_blender_solid_color brush(i->color.r, i->color.g, i->color.b, i->color.a);
-            unique_ptr<async_t::rasterizer_type> ras(_async.acquire());
+				unique_ptr<async_t::rasterizer_type> ras(_async.acquire());
 
 				ras->reset();
 
 				add_path(*ras, e);
 				_async.submit(ras.get(), surface, 0, brush);
-            ras.release();
+				ras.release();
 			}
+			_async.wait_completion();
 			timings.rendition += stopwatch(counter);
 		}
 
